0772cc0770162e701e37bc841fd13631b5c12988
Ty Overby <ty@pre-alpha.com>
2016-05-02 09:10:28 -0700

    Add drag region

--- examples/playground.rs
+++ examples/playground.rs
@@ -4,26 +4,32 @@ extern crate tabula;
 extern crate tabula_lux;
 
 use lux::prelude::*;
-use lux::color;
 use tabula::components::*;
-use tabula::{Rect, Point, Id};
+use tabula::Rect;
 
 fn main() {
     let mut window = Window::new_with_defaults().unwrap();
     let mut ui_ctx = tabula::UnloadedUiContext::new();
 
     let mut slider_pos = 0.5;
+    let mut region_rect = Rect::xywh(200.0, 200.0, 100.0, 100.0);
 
     while window.is_open() {
         ui_ctx.feed_events_for_frame(window.events().filter_map(::tabula_lux::translate_event));
         let mut frame = window.cleared_frame(rgb(0.9, 0.9, 0.9));
-        let mut backend = tabula_lux::DebugLuxBackend(&mut frame);
+        let backend = tabula_lux::DebugLuxBackend(&mut frame);
         let mut ui_ctx = ui_ctx.load(backend);
 
+        // Button
         if button(&mut ui_ctx, id!(), Rect::xywh(0.0, 0.0, 100.0, 100.0), "hello world").unwrap() {
             println!("clicked");
         }
 
+        // Slider
         slider_pos = slider(&mut ui_ctx, id!(), Rect::xywh(150.0, 0.0, 200.0, 50.0), slider_pos).unwrap();
+
+        // Drag Region
+        drag_region(&mut ui_ctx, id!(), &mut region_rect).unwrap();
+
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -10,16 +10,16 @@ pub struct DebugLuxBackend<'a>(pub &'a mut Frame);
 
 pub fn translate_event(e: LuxEvent) -> Option<TabulaEvent> {
     match e {
-        LuxEvent::MouseMoved((x, y)) => Some(TabulaEvent::PointerMove(EventSource(0), x as f32, y as f32)),
-        LuxEvent::MouseDown(_) => Some(TabulaEvent::PointerDown(EventSource(0))),
-        LuxEvent::MouseUp(_) => Some(TabulaEvent::PointerUp(EventSource(0))),
+        LuxEvent::MouseMoved((x, y)) => Some(TabulaEvent::PointerMove(EventSource(0, 0), x as f32, y as f32)),
+        LuxEvent::MouseDown(_) => Some(TabulaEvent::PointerDown(EventSource(0, 0))),
+        LuxEvent::MouseUp(_) => Some(TabulaEvent::PointerUp(EventSource(0, 0))),
         _ => None
     }
 }
 
 impl <'a> tabula::components::ButtonRender for DebugLuxBackend<'a> {
     type Error = ();
-    fn draw_button(&mut self, _id: Id, rect: Rect<f32>, _label: &str, hovered: bool, pressed: bool) -> Result<(), Self::Error> {
+    fn draw_button(&mut self, _id: &Id, rect: Rect<f32>, _label: &str, hovered: bool, pressed: bool) -> Result<(), Self::Error> {
         let Point(x, y) = rect.0;
         let (w, h) = (rect.width(), rect.height());
 
@@ -40,15 +40,30 @@ impl <'a> tabula::components::SliderRender for DebugLuxBackend<'a> {
 
     fn draw_slider(
         &mut self,
-        id: Id,
+        _id: Id,
         rect: Rect<f32>,
         covering: f32) -> Result<(), Self::Error> {
         let Point(x, y) = rect.0;
         let (w, h) = (rect.width(), rect.height());
 
         self.0.rect(x, y, w, h).color(lux::color::BLUE).fill();
-        println!("w {} cov {} {}", w, covering, w * covering);
         self.0.rect(x, y, w * covering, h).color(lux::color::RED).fill();
+        Ok(())
+    }
+}
+
+impl <'a> tabula::components::DragRegionRender for DebugLuxBackend<'a> {
+    type Error = ();
+
+    fn draw_drag_region(&mut self, _id: &Id, rect: Rect<f32>, drag_action: tabula::components::DragAction) -> Result<(), Self::Error> {
+        use tabula::components::DragAction;
+        let Point(x, y) = rect.0;
+        let (w, h) = (rect.width(), rect.height());
+        let color = match drag_action {
+            DragAction::None => lux::color::RED,
+            _ => lux::color::BLUE,
+        };
+        self.0.rect(x, y, w, h).color(color).fill();
         Ok(())
     }
 }
